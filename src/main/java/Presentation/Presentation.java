f185ce8e0f6acdcafc28f1a6a713cbb4ea1ef0a4
AmineBouizergane <aminebouizergane52@gmail.com>
2021-03-23 16:57:39 +0100

    injection avec spring beans xml

--- src/main/java/Presentation/Presentation.java
+++ src/main/java/Presentation/Presentation.java
@@ -4,6 +4,8 @@ import DAO.Dao;
 import DAO.IDao;
 import Metier.IMetier;
 import Metier.Metier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -13,32 +15,10 @@ import java.util.Scanner;
 
 public class Presentation {
     public static void main(String[] args) {
-
-        try {
-            Scanner s = new Scanner(new File("C:\\Users\\lenovo\\IdeaProjects\\JEE_Atelier1\\src\\main\\resources\\config.txt"));
-            Class clDao = Class.forName(s.nextLine());
-            IDao dao = (IDao) clDao.newInstance();
-            Class clMetier = Class.forName(s.nextLine());
-            IMetier metier = (IMetier) clMetier.newInstance();
-
-            Method method = clMetier.getMethod("setDao", IDao.class);
-            method.invoke(metier, dao);
-
-            System.out.println("Votre salaire net est "+metier.CalculeSalaireNet());
-
-        } catch (FileNotFoundException | ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        }
-
-
+        ApplicationContext context=new
+                ClassPathXmlApplicationContext("configb.xml");
+        IMetier metier=(IMetier) context.getBean("metier");
+        System.out.println("Votre salaire net est "+metier.CalculeSalaireNet());
     }
 }
 
